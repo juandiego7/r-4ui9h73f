a5b852c413b48a1abaf3a69bc8e4a3360f96a71c
juan.goez <juan.goez@enlared.net.co>
2019-11-05 15:33:07 -0500

    recto4

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  
+  constructor(){
+    super();
+    this.state = {
+      text: ''
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" onChange={this.onChangeInput} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.text}</p>
       </div>
     );
+  }
+
+  onChangeInput = (e) => {
+    this.setState({
+      text: e.target.value
+    });
   }
 }
 
